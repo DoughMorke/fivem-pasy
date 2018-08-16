daa324e752ddcbdd22fd77147b6d510d2c5679ae
DoughMorke <v0ltxmonster@gmial.com>
2018-08-17 01:23:22 +0200

    pasy

---
+++ pasy/config.lua
@@ -0,0 +1,4 @@
+Cfg             = {}
+Cfg.DiffTrigger = 0.255 
+Cfg.MinSpeed    = 8.25 --THIS IS IN m/s
+Cfg.Strings     = { belt_on = '^3Pasy zostały ^5 zapięte^0.', belt_off = '^3Pasy zostały ^1 odpięte^0.' }
